0dce6552e4d324e4318065adbdc583be6fbbf87a
Subham Kaumar <patelsk7481@gmail.com>
2025-04-11 13:05:11 +0530

    Initial Commit

---
+++ ProfileApp/ProfileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileAppApp.swift
+//  ProfileApp
+//
+//  Created by Subham Patel on 11/04/25.
+//
+
+import SwiftUI
+
+@main
+struct ProfileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProfileAppTests/ProfileAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileAppTests.swift
+//  ProfileAppTests
+//
+//  Created by Subham Patel on 11/04/25.
+//
+
+import Testing
+@testable import ProfileApp
+
+struct ProfileAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
